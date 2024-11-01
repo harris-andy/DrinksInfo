7f2bad1c42d5b72b5e859f2546369d99b420a752
harris-andy <agharris@gmail.com>
2024-11-01 12:05:15 -0400

    "save point for cleaning up comments, removing unnecessary Using statements and converting to file-scoped namespace"

--- Class_Functions/DisplayData.cs
+++ Class_Functions/DisplayData.cs
@@ -6,69 +6,57 @@ using System.Threading.Tasks.Dataflow;
 using DrinksInfo.Class_Objects;
 using Spectre.Console;
 
-namespace DrinksInfo;
-
-public class DisplayData
+namespace DrinksInfo
 {
-    public void ShowTable<T>(List<T> items, string title, Func<T, string> getID, Func<T, string> getName)
+    public class DisplayData
     {
-        var table = new Table();
-        bool isAlternateRow = false;
-        table.Title(title);
-        table.BorderColor(Color.DarkSlateGray1);
-        table.Border(TableBorder.Rounded);
-        table.AddColumn(new TableColumn("[cyan1]ID[/]").LeftAligned());
-        table.AddColumn(new TableColumn("[green1]Category[/]").RightAligned());
-
-        foreach (T item in items)
+        public void ShowTable<T>(List<T> items, string title, Func<T, string> getID, Func<T, string> getName)
         {
-            var color = isAlternateRow ? "grey" : "blue";
-            table.AddRow(
-                $"[{color}]{getID(item)}[/]",
-                $"[{color}]{getName(item)}[/]"
-            );
-            isAlternateRow = !isAlternateRow;
+            var table = new Table();
+            bool isAlternateRow = false;
+            table.Title(title);
+            table.BorderColor(Color.DarkSlateGray1);
+            table.Border(TableBorder.Rounded);
+            table.AddColumn(new TableColumn("[cyan1]ID[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[green1]Category[/]").RightAligned());
+
+            foreach (T item in items)
+            {
+                var color = isAlternateRow ? "grey" : "blue";
+                table.AddRow(
+                    $"[{color}]{getID(item)}[/]",
+                    $"[{color}]{getName(item)}[/]"
+                );
+                isAlternateRow = !isAlternateRow;
+            }
+            Console.Clear();
+            AnsiConsole.Write(table);
         }
-        Console.Clear();
-        AnsiConsole.Write(table);
+
+        public void ShowDrinkDetails(Drink drinkInfo)
+        {
+            var table = new Table();
+            string ingredientList = string.Join("\n", drinkInfo.CombinedIngMsrList.Select(pair => $"• {pair}"));
+            string formattedInstructions = string.Join("\n• ",
+                    drinkInfo.Instructions.Split(". ")
+                    .Select(sentence => sentence.Trim()));
+            formattedInstructions = "• " + formattedInstructions;
+            table.Title($"{drinkInfo.StrDrink} Details");
+            table.BorderColor(Color.DarkSlateGray1);
+            table.Border(TableBorder.Rounded);
+            table.AddColumn(new TableColumn("[cyan1]Name[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[green1]Instructions[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[blue1]Ingredients[/]").LeftAligned());
+            table.AddColumn(new TableColumn("[yellow1]Glass[/]").RightAligned());
+
+            table.AddRow(
+                $"[grey]{drinkInfo.StrDrink}[/]",
+                $"[blue]{formattedInstructions}[/]",
+                $"[grey]{ingredientList}[/]",
+                $"[blue]{drinkInfo.Glass}[/]"
+            );
+            Console.Clear();
+            AnsiConsole.Write(table);
+        }
     }
-
-    public void NothingFound(string item)
-    {
-        Console.Clear();
-        Console.WriteLine($"No {item} found!");
-        Thread.Sleep(2000);
-    }
-
-    public void ShowDrinkDetails(Drink drinkInfo)
-    {
-        var table = new Table();
-        string ingredientList = string.Join("\n", drinkInfo.CombinedIngMsrList.Select(pair => $"• {pair}"));
-        string formattedInstructions = string.Join("\n• ",
-                drinkInfo.Instructions.Split(". ")
-                .Select(sentence => sentence.Trim()));
-        formattedInstructions = "• " + formattedInstructions;
-        table.Title($"{drinkInfo.StrDrink} Details");
-        table.BorderColor(Color.DarkSlateGray1);
-        table.Border(TableBorder.Rounded);
-        table.AddColumn(new TableColumn("[cyan1]Name[/]").LeftAligned());
-        table.AddColumn(new TableColumn("[green1]Instructions[/]").LeftAligned());
-        table.AddColumn(new TableColumn("[blue1]Ingredients[/]").LeftAligned());
-        table.AddColumn(new TableColumn("[yellow1]Glass[/]").RightAligned());
-        // table.AddColumn(new TableColumn("[red1]% Correct[/]").LeftAligned());
-
-        // var color = isAlternateRow ? "grey" : "blue";
-        table.AddRow(
-            // $"[{color}]{record.Date.ToShortDateString()}[/]",
-            $"[grey]{drinkInfo.StrDrink}[/]",
-            $"[blue]{formattedInstructions}[/]",
-            $"[grey]{ingredientList}[/]",
-            $"[blue]{drinkInfo.Glass}[/]"
-        );
-        // isAlternateRow = !isAlternateRow;
-
-        Console.Clear();
-        AnsiConsole.Write(table);
-    }
-
 }
